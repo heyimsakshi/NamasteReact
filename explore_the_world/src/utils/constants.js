349f49bd0e136ed12d7a51caf66ff4d00481727e
Sakshi Agrawal <shakshimnnit@gmail.com>
2024-02-04 13:29:28 +0530

    explore the world started

---
+++ explore_the_world/src/utils/constants.js
@@ -0,0 +1,5 @@
+//Below written are the named exports since there are multiple URL links
+
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/"
+
+export const LOGO_URL="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
