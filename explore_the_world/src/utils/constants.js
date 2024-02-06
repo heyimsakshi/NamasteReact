6a2114364245749fc81d600d18f78eadbacf3a95
Shakshi Agrawal <FE748@NSLHUB.COM>
2024-02-06 18:22:03 +0530

    Filter functionality done

--- explore_the_world/src/utils/constants.js
+++ explore_the_world/src/utils/constants.js
@@ -3,3 +3,5 @@
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/"
 
 export const LOGO_URL="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+
+export const API_URL="https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.456333351378966&lng=78.37263149036954&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
